08ddd59dd3be91ad39e05dd269eab9318c3a2660
JosephSsanja <joevega.sanju6ss@gmail.com>
2024-02-16 13:39:31 +0000

    json-server downgrade

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const fs = require('fs');
-const pause = require('connect-pause');
+
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
@@ -11,12 +10,11 @@ server.use(jsonServer.bodyParser);
 //
 // Define custom routes (routes.json)
 //
-var routes = JSON.parse(fs.readFileSync('routes.json'));
-server.use(jsonServer.rewriter(routes));
 
 
-server.use(pause(1000));
+
+
 server.use(router);
-server.listen(8008, () => {
+server.listen(8080, () => {
   console.log('JSON Server is running');
 });
